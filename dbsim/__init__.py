8d1c957f0dad9ddd7e9e3146f3aa9e4d473ee152
Michael Mior <mmior@cs.rit.edu>
2022-11-09 08:59:59 -0500

    Fix import order

--- dbsim/__init__.py
+++ dbsim/__init__.py
@@ -1,8 +1,9 @@
 __version__ = "1.0"
 
 from .ast import Relation
-from .dataset import DataSet
 from .field import Field, FieldType
-from .query import Query
 from .schema import Schema
+
+from .query import Query
 from .table import Table
+from .dataset import DataSet
